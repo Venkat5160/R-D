9572d1193fe18f21e7d8ebd4be9071edfa875679
Venkat5160 <venkat.5160@gmail.com>
2017-06-06 20:01:31 +0530

    TinyMce
    Intigrating TinyMce Html Editot with Asp.Net MVC

---
+++ TinyMCE/TinyMCE/Controllers/TinyMCE_DemoController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TinyMCE.Models;
+
+namespace TinyMCE.Controllers
+{
+    public class TinyMCE_DemoController : Controller
+    {
+        //
+        // GET: /TinyMCE_Demo/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(TinyDemoModel model)
+        {
+            //var htmlDoc = new HtmlDocument();
+            //var tempModel = model.Template;
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["temp"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("spInserttemplate", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Template", model.Template);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return View();
+        }
+
+        public ActionResult Display()
+        {
+
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["temp"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("spGettemplate", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            TinyDemoModel model;
+            List<TinyDemoModel> lst = new List<TinyDemoModel>();
+            while (dr.Read())
+            {
+                model = new TinyDemoModel();
+                model.Id=Convert.ToInt32(dr["Id"]);
+                model.Template = Convert.ToString(dr["Body"]);
+                lst.Add(model);
+
+            }
+            con.Close();
+            return View(lst);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["temp"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("spEdittemplate", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", id);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            TinyDemoModel model = new TinyDemoModel();
+            if (dr.Read())
+            {
+                model.Template = Convert.ToString(dr["Body"]);
+
+            }
+            con.Close();
+            return View(model);
+        }
+    }
+}
---
+++ TinyMCE/TinyMCE/Models/TinyDemoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TinyMCE.Models
+{
+    public class TinyDemoModel
+    {
+        public int Id { get; set; }
+        [UIHint("tinymce_jquery_full")]
+        [AllowHtml]
+        public string Template { get; set; }
+    }
+}
